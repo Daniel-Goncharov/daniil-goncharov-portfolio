3d348d75851e3181aeffa8092183c324f8fb6b95
Daniel Goncharov <goncharov10d@gmail.com>
2022-11-23 14:36:18 +0500

    first commit

---
+++ utils/constants.js
@@ -0,0 +1,26 @@
+export const footerButton = document.querySelector('.menu-burger__button');
+export const footerPopup = document.querySelector('.burger-menu');
+export const footerSocial = document.querySelector('.footer__column_content_social');
+export const footerInfo = document.querySelector('.footer__column_content_info');
+export const commentTemplate = document.querySelector('.comment').content.querySelector('.comment__conteiner');
+export const commentForm = document.querySelector('.form');
+export const nameImput = document.querySelector('.form__item_el_username');
+export const textImput = document.querySelector('.form__item_el_comment');
+export const buttonSubmit = document.querySelector('.button-submit');
+export const buttonReset = document.querySelector('.button-reset');
+export const commentContainer = document.querySelector('.comments');
+
+export const commentsItems = [
+  {
+    userName: 'Вовка из Тридевятого царства',
+    userComment: 'Не хочу! В школе учат учат, еще и тут в интернете навалились!!'
+  },
+  {
+    userName: 'Незнайка',
+    userComment: 'Да! Я в интернет захожу, не для того, чтобы про всякий Файнеменов читать!'
+  },
+  {
+    userName: 'Знайка',
+    userComment: 'Ну вы и балбесы ;)'
+  }
+];
