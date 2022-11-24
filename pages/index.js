fbb549f55a3d6c78ed7748b38f1fc784f225feca
Daniel Goncharov <goncharov10d@gmail.com>
2022-11-24 16:44:54 +0500

    feat: добавлено переключение языка сайта

--- pages/index.js
+++ pages/index.js
@@ -19,3 +19,47 @@ function slidesPlugin(activeSlide = 0) {
 }
 
 slidesPlugin(1);
+
+const langBtns = document.querySelectorAll('.lang-menu__btn');
+const allLang = ['en', 'ru'];
+const enBtn = document.querySelector('.enBtn');
+const ruBtn = document.querySelector('.ruBtn');
+
+function setLanguageIndicator(lang) {
+  if (lang === 'en') {
+    enBtn.classList.add('lang-menu__btn_select');
+    ruBtn.classList.remove('lang-menu__btn_select');
+  } else {
+    ruBtn.classList.add('lang-menu__btn_select');
+    enBtn.classList.remove('lang-menu__btn_select');
+  }
+}
+
+langBtns.forEach((item) => {
+  item.addEventListener('click', changeUrlLanguage);
+});
+
+function changeUrlLanguage(evt) {
+  let lang = evt.target.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+};
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  setLanguageIndicator(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  };
+  for (let key in langArr) {
+    let elem = document.querySelector('.lng-' + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+  };
+};
+
+changeLanguage();
+
--- utils/constants.js
+++ utils/constants.js
@@ -1,26 +1,102 @@
-export const footerButton = document.querySelector('.menu-burger__button');
-export const footerPopup = document.querySelector('.burger-menu');
-export const footerSocial = document.querySelector('.footer__column_content_social');
-export const footerInfo = document.querySelector('.footer__column_content_info');
-export const commentTemplate = document.querySelector('.comment').content.querySelector('.comment__conteiner');
-export const commentForm = document.querySelector('.form');
-export const nameImput = document.querySelector('.form__item_el_username');
-export const textImput = document.querySelector('.form__item_el_comment');
-export const buttonSubmit = document.querySelector('.button-submit');
-export const buttonReset = document.querySelector('.button-reset');
-export const commentContainer = document.querySelector('.comments');
-
-export const commentsItems = [
-  {
-    userName: 'Вовка из Тридевятого царства',
-    userComment: 'Не хочу! В школе учат учат, еще и тут в интернете навалились!!'
+const langArr = {
+  "menu-main" : {
+    "ru": "Главное",
+    "en": "Main"
   },
-  {
-    userName: 'Незнайка',
-    userComment: 'Да! Я в интернет захожу, не для того, чтобы про всякий Файнеменов читать!'
+  "menu-about" : {
+    "ru": "Кто я",
+    "en": "Who am I"
   },
-  {
-    userName: 'Знайка',
-    userComment: 'Ну вы и балбесы ;)'
-  }
-];
+  "menu-projects" : {
+    "ru": "Мои проекты",
+    "en": "My projects"
+  },
+  "menu-contact" : {
+    "ru": "Контакты",
+    "en": "Contact"
+  },
+  "name" : {
+    "ru": "Даниил Гончаров",
+    "en": "Daniil Goncharov"
+  },
+  "job" : {
+    "ru": "Веб-разработчик",
+    "en": "Web-developer"
+  },
+  "about" : {
+    "ru": "Кто я",
+    "en": "Who am I"
+  },
+  "short-description" : {
+    "ru": "Привет! Я веб-разработчик, занимаюсь версткой адаптивных сайтов. Хорошо знаю Java Script и React.",
+    "en": "Hi! I am a web developer, I make responsive websites. I am good at Java Script and React."
+  },
+  "long-description-one" : {
+    "ru": "Родился в Москве. В 2012 году закончил Российский Государственный Гуманитарный Университет по специальности социальная психология. 10 лет работал в сфере развития и обучения персонала. Разрабатывал и проводил учебные курсы, тренинги, дистанционные он-лайн курсы. В 2021 году перешел работать в IT стартап где занимался «Управлением знаниями», а именно писал документацию для разработчиков по софту,который разрабатывала компания. Организовал учебный портал для хранения этой информации и для этого начал изучать html верстку.",
+    "en": "I was born in Moscow. In 2012 I graduated from the Russian State University for the Humanities with a degree in social psychology. 10 years of experience in staff development and training. Developed and conducted training courses, trainings, distance online courses. In 2021, I moved to work at an IT startup where I was engaged in Knowledge Management, specifically, I wrote software documentation of methodological and training materials for clients using the software created by the company. I organized an educational portal for storing this information and for this I began to study html layout."
+  },
+  "long-description-two" : {
+    "ru": "В 2022 году решил, что хочу полностью посветить себя разработке сайтов и прошел учебный курс Яндекс Практику, чтобы стать сертефицированным Веб-разработчиком.",
+    "en": "In 2022, I decided expanding my career into web development. Finishing the Software Engineer course at Yandex Practicum to become a certified Web Developer."
+  },
+  "skills" : {
+    "ru": "Мои навыки",
+    "en": "My skills"
+  },
+  "projects" : {
+    "ru": "Мои проекты",
+    "en": "My projects"
+  },
+  "how-to-learn" : {
+    "ru": "Научиться учиться",
+    "en": "How to learn"
+  },
+  "russian-travel" : {
+    "ru": "Путишествие по России",
+    "en": "Russian travel"
+  },
+  "mesto" : {
+    "ru": "проект Место",
+    "en": "project Mesto"
+  },
+  "aim-training" : {
+    "ru": "игра Тренировка меткости",
+    "en": "Aim-Training game"
+  },
+  "vertical-slider" : {
+    "ru": "сайт с вертикальным слайдером изображений",
+    "en": "website with Vertical Slider"
+  },
+  "contact" : {
+    "ru": "Контакты",
+    "en": "Contact"
+  },
+  "linkedin" : {
+    "ru": "Моя страница в Linkedin",
+    "en": "My page in Linkedin"
+  },
+  "telegram" : {
+    "ru": "Мой Telegram",
+    "en": "My Telegram"
+  },
+  "view-website-1" : {
+    "ru": "Посмотреть сайт &rarr;",
+    "en": "View website &rarr;"
+  },
+  "view-website-2" : {
+    "ru": "Посмотреть сайт &rarr;",
+    "en": "View website &rarr;"
+  },
+  "view-website-3" : {
+    "ru": "Посмотреть сайт &rarr;",
+    "en": "View website &rarr;"
+  },
+  "view-website-4" : {
+    "ru": "Посмотреть сайт &rarr;",
+    "en": "View website &rarr;"
+  },
+  "view-website-5" : {
+    "ru": "Посмотреть сайт &rarr;",
+    "en": "View website &rarr;"
+  },
+};
